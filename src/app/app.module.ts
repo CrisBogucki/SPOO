b667c1f9fd7f01b9d30aced17c4c8caaf5405ec5
crisb <krzysztof.lubien@icloud.com>
2021-03-07 18:08:47 +0100

    zmiana strategii path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {HttpJwtInterceptor} from './@core/interceptors/http-jwt.interceptor';
 import {HttpErrorInterceptor} from './@core/interceptors/http-error.interceptor';
 
-import {APP_BASE_HREF, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from '@angular/common';
 import {AppConfig} from './@config/app.config';
 import {environment} from '../environments/environment';
 
@@ -27,7 +27,7 @@ import {SessionService} from './@core/services/session.service';
         AppConfig, SessionConfig, SessionService,
         {provide: HTTP_INTERCEPTORS, useClass: HttpJwtInterceptor, multi: true},
         {provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true},
-        {provide: LocationStrategy, useClass: PathLocationStrategy},
+        {provide: LocationStrategy, useClass: HashLocationStrategy},
         {provide: APP_BASE_HREF, useValue: environment.app_base_href},
         authorizationMockService
     ],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost',
   app_base_href: 'https://spoo.netlify.com/',
-  useLocalStorage: false
+  useLocalStorage: true
 };
